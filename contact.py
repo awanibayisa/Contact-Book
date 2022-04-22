ba1f4865f5bc1ffd16ed6535b8f9299d61108d08
awanibayisa <rahmedania2022@gmail.com>
2022-04-22 05:02:30 +0500

    docstrings and basic structure

---
+++ contact.py
@@ -0,0 +1,34 @@
+# Import Module
+from tkinter import *
+
+def create_db():
+    """
+    this method will create database if not created
+    :return:
+    """
+
+def add():
+    """
+    this method will add contact details to the database
+    :return:
+    """
+
+def view():
+    """
+    this method will retrieve details for selected contact and show on screen
+    :return:
+    """
+def delete():
+    """
+    this method will delete contact details of selected contact
+    :return:
+    """
+
+def create_GUI():
+    """
+    this method will create GUI for the app
+    :return:
+    """
+
+
+if __name__ == "__main__":
