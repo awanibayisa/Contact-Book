08ed6df772742ed1efc30a2bb99035bcf467e501
awanibayisa <rahmedania2022@gmail.com>
2022-04-22 05:09:52 +0500

    test cases structure

---
+++ tests.py
@@ -0,0 +1,19 @@
+import unittest
+from contact import *
+
+class TestContactMethods(unittest.TestCase):
+
+    def test_create_GUI(self):
+        pass
+
+    def test_add(self):
+        pass
+
+    def test_view(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_create_db(self):
+        pass
