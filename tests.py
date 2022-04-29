b82a09b37540f3a261fd387f6c128317a1d65865
awanibayisa <rahmedania2022@gmail.com>
2022-04-30 02:13:54 +0500

    tests for gui and database added

--- tests.py
+++ tests.py
@@ -4,6 +4,13 @@ from contact import *
 class TestContactMethods(unittest.TestCase):
 
     def test_create_GUI(self):
+        """
+        we cannot test this method automatically. We need to do it manually. To test this, follow these steps.
+        1) Run the app.
+        2) Check if all the components are visible
+        3) Check if all the buttons and functions working
+        :return:
+        """
         pass
 
     def test_add(self):
@@ -16,4 +23,11 @@ class TestContactMethods(unittest.TestCase):
         pass
 
     def test_create_db(self):
+        """
+        this method cannot be tested automatically. We need to do it manually. To test this, follow these steps.
+        1) Run the app.
+        2) Go to the directory where your app resides
+        3) If file with name contacts.db is there, then test succeed
+        :return:
+        """
         pass
