9e8f6f18617db62ea6edfdc0668cd6395dfb7454
awanibayisa <rahmedania2022@gmail.com>
2022-05-14 01:20:05 +0500

    methods implementation completed

--- contact.py
+++ contact.py
@@ -7,12 +7,12 @@ def create_db():
     this method will create database if not created
     :return:
     """
+    
     conn = sqlite3.connect('contact.db')
-    conn.execute('''CREATE TABLE CONTACTS
-             (ID INT PRIMARY KEY     NOT NULL,
-             NAME           TEXT    NOT NULL,
+    conn.execute('''CREATE TABLE IF NOT EXISTS CONTACTS
+             (NAME           TEXT    NOT NULL,
              NUMBER            INT     NOT NULL,
-             ADDRESS        CHAR(50);''')
+             ADDRESS        CHAR(50));''')
     conn.close()
 
 def add():
@@ -20,24 +20,77 @@ def add():
     this method will add contact details to the database
     :return:
     """
+    global Name, Number, address
+
+    conn = sqlite3.connect('contact.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO CONTACTS (NAME, NUMBER, ADDRESS) VALUES (?, ?, ?)", (Name.get(),Number.get(),address.get(1.0, "end-1c")))
+
+    update()
+
+    conn.commit()
+    conn.close()
+
+    Name.set('')
+    Number.set('')
+    address.delete(1.0, "end")
+
+def update():
+    """
+    this method will update the list of contacts
+    :return:
+    """
+    global select
+    conn = sqlite3.connect('contact.db')
+    cursor = conn.cursor()
+    select.delete(0, END)
+    cursor.execute("SELECT NAME from CONTACTS")
+    for row in cursor:
+        select.insert(END, row[0])
+    conn.close()
 
 def view():
     """
     this method will retrieve details for selected contact and show on screen
     :return:
     """
+    global select, Name, Number, address
+    selected = select.get(select.curselection()[0])
+    conn = sqlite3.connect('contact.db')
+    cursor = conn.execute("SELECT * from CONTACTS WHERE NAME = ?", (selected,))
+    row = cursor.fetchone()
+
+    Name.set(str(row[0]))
+    Number.set(str(row[1]))
+    address.insert(1.0, str(row[2]))
+
+    conn.close()
+
+
+
 def delete():
     """
     this method will delete contact details of selected contact
     :return:
     """
+    global select, Name, Number, address
+    selected = select.get(select.curselection()[0])
+    conn = sqlite3.connect('contact.db')
+    conn.execute("DELETE from CONTACTS WHERE NAME = ?", (selected,))
+
+    conn.commit()
+    conn.close()
+
+    update()
+
 
 def create_GUI():
     """
     this method will create GUI for the app
     :return:
     """
-
+    global Name, Number, address, select
+    create_db()
     root = Tk()
     root.title("Contact Book")
     root.geometry('400x430')
@@ -65,6 +118,7 @@ def create_GUI():
     scroll_bar.pack(side=RIGHT, fill=Y)
     select.place(x=20, y=170)
 
+    update()
     # Execute Tkinter
     root.mainloop()
 
